14d03b824687a231d61b8406f9506a78793169f2
richardson wigner <richardsonman@hotmail.com>
2020-12-02 11:28:17 -0300

    Adicionar arquivos de projeto.

---
+++ Try_Catch_Listas_Sobrecarga/Data.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Try_Catch_Listas_Sobrecarga
+{
+    class Data
+    {
+        List<Data> datas = new List<Data>();
+        int dia;
+        int mes;
+        int ano;
+
+        public Data()
+        {
+           
+        }
+        public Data(int dia, int mes, int ano)
+        {
+            this.dia = dia;
+            this.mes = mes;
+            this.ano = ano;
+        }
+        public int Dia
+        {
+            get { return dia; }
+            set { dia = value; }
+        }
+        public int Mes
+        {
+            get { return mes; }
+            set { mes = value; }
+        }
+        public int Ano
+        {
+            get { return ano; }
+            set { ano = value; }
+        }
+        public List<Data> Datas
+        {
+            get { return datas; }
+        }
+        public static void DataAtual()
+        {          
+            Console.WriteLine(DateTime.Today.ToString());
+        }
+        public List<Data> DatasAdicionadas(List<Data> datas)
+        {
+            return datas.Where(x => x.Contains(datas)).ToList();
+        }
+
+      
+    }
+}
---
+++ Try_Catch_Listas_Sobrecarga/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Try_Catch_Listas_Sobrecarga
+{
+    class Program
+    {
+        static Data diaAtual = new Data();
+        static void Main(string[] args)
+        {
+            PedirUsuario();
+          
+        }
+        public static void PedirUsuario()
+        {
+            Console.WriteLine(@$"O que deseja fazer?
+            1:Ver Data Atual
+            2:Adicionar Data");
+            if(diaAtual.Datas.Count != 0)
+            {
+                Console.WriteLine("3:Ver Datas Adicionadas");
+            }
+            string PedidoUsuario = Console.ReadLine();
+            if(PedidoUsuario == "1")
+            {
+                Data.DataAtual();
+            }
+            else if (PedidoUsuario == "2")
+            {
+                AdicionarData();
+            }
+            else if (PedidoUsuario == "3")
+            {
+                Data.DatasAdicionadas();
+            }
+        }
+        public static void AdicionarData()
+        {
+            bool Loop = true;
+            while (Loop == true)
+            {
+                try
+                {
+                    Console.WriteLine("Adicione o dia");
+                    int Dia = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Adicione o mês");
+                    int Mes = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Adicione o ano");
+                    int Ano = int.Parse(Console.ReadLine());
+                    diaAtual.Datas.Add(new Data(Dia, Mes, Ano));
+                    Loop = false;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Data Inválida");
+                }
+            }
+        }
+    }
+}
